ba12c322a8faa07c99f6d4b82138890809070830
Goodness Okanlawon <102490495+CodeEvolver@users.noreply.github.com>
2025-03-21 15:21:52 +0100

    RandomId becomes const

--- app/random-post/page.tsx
+++ app/random-post/page.tsx
@@ -3,12 +3,24 @@
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
+export type Post = {
+  id: number,
+  userId: number,
+  title: string,
+  body: string,
+}
+
 export default function RandomPost() {
-  const [randomPost, setRandomPost] = useState<any | null>({
-    id: 1,
-    title: "Zustand for data management",
-    content: "Zustand has been a go to library for managing data in medium sized react applications, it's light weight, fast, and easy to use. However, setting it up for NEXTjs can be a thorn a in the flesh."
-  });
+  const [posts, setPosts] = useState<Post[] | null>([]);
+
+  const [randomPost, setRandomPost] = useState<Post | null>(null);
+
+  // {
+  //   id: 1,
+  //   userId: 1,
+  //   title: "Zustand for data management",
+  //   body: "Zustand has been a go to library for managing data in medium sized react applications, it's light weight, fast, and easy to use. However, setting it up for NEXTjs can be a thorn a in the flesh."
+  // }
 
   const url = usePathname();
   const router = useRouter();
@@ -17,22 +29,17 @@ export default function RandomPost() {
 
   const endpoint = "https://jsonplaceholder.typicode.com/posts";
 
-  const getRandomPost = async() => {
+  const generateRandomNumber = (range: number) => {
+    return Math.floor(Math.random() * (range + 1));
+  }
+
+  const getPosts = async() => {
     setIsLoading(true);
     try {
-      await fetch(endpoint, {
-        method: "GET",
-      }).then(
-        (res) => {
-          res.json();
-          console.log("first then");
-        }
-      ). then(
-        (res) => {
-          setRandomPost(res);
-          console.log("second then");
-        }
-      )
+      const response = await fetch(endpoint)
+      const data = await response.json()
+      setPosts(data)
+      console.log(data)
     } catch(error) {
       console.log(error);
     } finally {
@@ -40,9 +47,21 @@ export default function RandomPost() {
     }
   }
 
+  const getRandomPost = () => {
+    if(!posts?.length) {
+      return
+    }
+    let randomId = generateRandomNumber(posts?.length);
+    setRandomPost(posts[randomId]);
+  }
+
   useEffect(() => {
-    getRandomPost()
-  }, [])
+    if(posts) {
+      getRandomPost();
+    } else {
+      getPosts();
+    }
+  }, [posts])
 
   return (
     <div className="w-[100vw] max-w-[100%] h-[100vh] flex flex-col items-center justify-center p-2">
@@ -53,15 +72,15 @@ export default function RandomPost() {
         isLoading? <p>Loading...</p>:
         !isLoading && !randomPost? <div className="flex flex-col items-center gap-2">
           <p>Error fetching post</p>
-          <button onClick={() => getRandomPost()} className="w-[100%] p-2 rounded-md bg-white text-black">Reload</button>
+          <button onClick={() => getPosts()} className="w-[100%] p-2 rounded-md bg-white text-black">Reload</button>
         </div>:
         <div className="w-[100%] max-w-[450px] rounded-md flex flex-col items-center justify-center gap-3 p-3 bg-white text-black">
           <div className="w-[100%] flex flex-row items-center justify-start gap-2">
-            <p>{randomPost?.id + " |"}</p>
+            <p className="text-nowrap">{randomPost?.id + " |"}</p>
             <p className="text-black font-semibold text-xl">{randomPost?.title}</p>
           </div>
           <div className="w-[100%] text-start">
-            <p>{randomPost?.content}</p>
+            <p>{randomPost?.body}</p>
           </div>
           <div className="w-[100%] flex flex-row items-center justify-end">
             <button onClick={() => getRandomPost()} className="w-[100px] p-2 rounded-md border-[1px] border-black">
