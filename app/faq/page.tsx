24641b6d574ecda3acda7aa3dd7373684c723955
Goodness Okanlawon <102490495+CodeEvolver@users.noreply.github.com>
2025-03-22 13:23:29 +0100

    UI Engineering Completed

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,6 +1,9 @@
 "use client"
 import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
+import { BiArrowBack, BiMinusCircle, BiPlusCircle, BiStar } from "react-icons/bi";
+import { BsStar } from "react-icons/bs";
+import { SiTwinkly } from "react-icons/si";
 
 export default function Faq() {
   const [openedQuestion, setOpenedQuestion] = useState<number | null>(1);
@@ -41,21 +44,21 @@ export default function Faq() {
   return (
     <div className="w-100vw max-w-[100%] h-[100vh] flex flex-row items-center justify-center p-2 bg-gradient-to-b from-[--vibrant-dark] from-[1%] to-[--vibrant-light] to-[99%]">
       <header className="fixed w-[100vw] max-w-[100%] p-1 sm:p-2 md:p-3 top-0 left-0 flex flex-row items-center justify-start">
-          {url.endsWith("faq") || url.endsWith("random-post")? <button onClick={() => router.back()} className="text-white text-2xl">{"<"}</button>: null}
+          {url.endsWith("faq") || url.endsWith("random-post")? <button onClick={() => router.back()} className="text-white text-2xl"><BiArrowBack /></button>: null}
       </header>
-      <div className="w-[100%] max-w-[450px] flex flex-col items-center justify-center gap-2 p-4 rounded-md shadow-md text-black bg-[--dominant]">
-        <div className="w-[100%] flex flex-row items-center justify-start gap-1">
-          <span></span>
-          <h1 className="text-2xl font-bold text-[--vibrant-dark]">FAQs</h1>
+      <div className="w-[100%] max-w-[450px] flex flex-col items-center justify-center gap-2 p-4 rounded-md shadow-md bg-[--dominant] dark:bg-[--background]">
+        <div className="w-[100%] flex flex-row items-center justify-start gap-2">
+          <span className="text-[--vibrant]"><SiTwinkly /></span>
+          <h1 className="text-2xl font-bold text-[--vibrant-dark] dark:text-[--vibrant-light]">FAQs</h1>
         </div>
         <ul className="w-[100%] flex flex-col items-center justify-center gap-2">
           {faqs.map((faq) => 
             <li key={faq.id} className="w-[100%] flex flex-col p-1">
               <div className="w-[100%] flex flex-row items-start justify-between gap-1">
-                <p className="text-[--muted-dark] font-semibold">{faq.question}</p>
-                <span onClick={() => handleExpand(faq.id)} className={`cursor-pointer ${openedQuestion === faq.id? "text-[--vibrant-dark]": "text-[--vibrant]"} font-semibold text-center`}>{openedQuestion === faq.id? "-": "+"}</span>
+                <p className="text-[--muted-dark] dark:text-[--vibrant-light] font-semibold">{faq.question}</p>
+                <span onClick={() => handleExpand(faq.id)} className={`cursor-pointer ${openedQuestion === faq.id? "text-[--vibrant-dark] dark:text-[--vibrant-dark]": "text-[--vibrant] dark:text-[--vibrant]"} font-semibold text-center`}>{openedQuestion === faq.id? <BiMinusCircle />: <BiPlusCircle />}</span>
               </div>
-              {faq.id === openedQuestion && <p className="text-[--muted]">{faq.answer}</p>}
+              {faq.id === openedQuestion && <p className="text-[--muted] dark:text-[--muted-light]">{faq.answer}</p>}
             </li>
           )}
         </ul>
--- app/random-post/page.tsx
+++ app/random-post/page.tsx
@@ -2,6 +2,7 @@
 
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { BiArrowBack } from "react-icons/bi";
 
 export type Post = {
   id: number,
@@ -58,25 +59,25 @@ export default function RandomPost() {
 
 
   return (
-    <div className="w-[100vw] max-w-[100%] h-[100vh] flex flex-col items-center justify-center p-2 bg-[--vibrant-light]">
+    <div className="w-[100vw] max-w-[100%] h-[100vh] flex flex-col items-center justify-center p-2 bg-[--background]">
       <header className="fixed w-[100vw] max-w-[100%] p-1 sm:p-2 md:p-3 top-0 left-0 flex flex-row items-center justify-start">
-        {url.endsWith("faq") || url.endsWith("random-post")? <button onClick={() => router.back()} className="text-white text-2xl">{"<"}</button>: null}
+        {url.endsWith("faq") || url.endsWith("random-post")? <button onClick={() => router.back()} className="text-white text-2xl"><BiArrowBack /></button>: null}
       </header>
       {      
         isLoading? <p>Loading...</p>:
         !randomPost? <div className="flex flex-col items-center gap-2">
-          <p>Error fetching post</p>
-          <button onClick={() => getPosts()} className="w-[100%] p-2 rounded-md bg-white text-black">Reload</button>
+          <p>Error fetching post, don't fret!</p>
+          <button onClick={() => getPosts()} className="w-[16rem] max-w-[100%] p-2 rounded-md bg-[--foreground] text-[--vibrant]">Reload</button>
         </div>:
-        <div className="w-[100%] max-w-[450px] rounded-md flex flex-col items-center justify-center gap-3 p-3 bg-[--dominant] shadow-md">
+        <div className="w-[100%] max-w-[450px] rounded-md flex flex-col items-center justify-center gap-3 p-3 bg-[--dominant] dark:bg-[--vibrant-dark] shadow-md">
           <div className="w-[100%] flex flex-row items-center justify-start gap-2">
-            <p className="text-black font-semibold text-xl text-[--vibrant-dark]"><span className="text-[--vibrant]">{randomPost?.id}</span> {randomPost?.title}</p>
+            <p className="text-black font-semibold text-xl text-[--vibrant-dark] dark:text-[--vibrant-light]"><span className="text-[--vibrant]">{randomPost?.id}</span> {randomPost?.title}</p>
           </div>
-          <div className="w-[100%] text-start text-[--muted]">
+          <div className="w-[100%] text-start text-[--muted] dark:text-[--dominant]">
             <p>{randomPost?.body}</p>
           </div>
           <div className="w-[100%] flex flex-row items-center justify-end">
-            <button onClick={() => getRandomPost()} className="w-[100px] p-1 rounded-md border-[1px] border-[--vibrant] text-[--vibrant]">
+            <button onClick={() => getRandomPost()} className="w-[100px] p-1 rounded-md border-[1px] border-[--vibrant] dark:border-[--vibrant-light] text-[--vibrant] dark:text-[--vibrant-light]">
               New Post
             </button>
           </div>
